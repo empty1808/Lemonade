21203e8037d61e567d30f41ddb73810c8778c0a2
Lemon251206 <empty.1808@gmail.com>
2023-12-17 11:40:42 +0700

    11:40 - 17/12/2023

--- AnimeAdventures/class/Classes.lua
+++ AnimeAdventures/class/Classes.lua
@@ -1,7 +1,7 @@
 local modules = {};
 
 local function loadClass(class_name)
-    return loadstring(game:HttpGet('https://raw.githubusercontent.com/empty1808/Lemonade/main/AnimeAdventures/class'..tostring(class_name)))();
+    return loadstring(game:HttpGet('https://raw.githubusercontent.com/empty1808/Lemonade/main/AnimeAdventures/class/'..tostring(class_name)))();
 end
 
 modules.Players = loadClass('Players.lua');
